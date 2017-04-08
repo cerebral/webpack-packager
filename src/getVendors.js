9bb859cb601ac4b55dbbb12cf674ca5528cbe22d
Christian Alfoni <christianalfoni@gmail.com>
2017-04-09 00:05:30 +0200

    Clean entry points list

--- src/getVendors.js
+++ src/getVendors.js
@@ -18,5 +18,14 @@ module.exports = function (entries) {
 
         return entryPoints.concat(entryPointList);
       }, [])
-    });
+    })
+    .then(function (entryPoints) {
+      return entryPoints.reduce(function (allEntryPoints, entryPoint) {
+        if (allEntryPoints.indexOf(entryPoint) === -1) {
+          return allEntryPoints.concat(entryPoint);
+        }
+
+        return allEntryPoints;
+      }, []);
+    })
 }
