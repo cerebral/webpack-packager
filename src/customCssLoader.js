39d700a45f3ffe3e1640ee2958f7bf865ec60f6d
Christian Alfoni <christianalfoni@gmail.com>
2017-04-21 10:11:41 +0200

    Removed comment... haha

--- src/customCssLoader.js
+++ src/customCssLoader.js
@@ -1,7 +1,6 @@
 module.exports = function (content) {;
   return `
     module.exports = (function () {
-      console.log('wuuuut?');
       var styleTag = document.createElement('style');
       styleTag.type = 'text/css';
       styleTag.appendChild(document.createTextNode(${JSON.stringify(content)}));
