cc58bc15b76a3e8cc5426bc761586fc66b4c6b1d
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-04-22 14:31:11 +0200

    Ignore engines

--- src/extract.js
+++ src/extract.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 
 module.exports = function (packages, packagePath) {
   return new Promise(function (resolve, reject) {
-    exec(`mkdir -p ${packagePath} && cd ${packagePath} && yarn add ${packages.join(' ')} --no-lockfile --ignore-scripts --non-interactive --no-bin-links --no-lockfile`, function (err, stdout, stderr) {
+    exec(`mkdir -p ${packagePath} && cd ${packagePath} && yarn add ${packages.join(' ')} --no-lockfile --ignore-scripts --non-interactive --no-bin-links --no-lockfile --ignore-engines`, function (err, stdout, stderr) {
       if (err) {
         reject(err.message.indexOf('versions') >= 0 ? new Error('INVALID_VERSION') : err);
       } else {
